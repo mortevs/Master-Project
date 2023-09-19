2db0b4df9623a11977f5429afba6a4337de8014f
Morten Simensen <morten.viersi@gmail.com>
2023-09-19 15:57:50 +0200

    fixed bug with all plot

--- Plotting/multi_plot.py
+++ Plotting/multi_plot.py
@@ -1,4 +1,5 @@
 import plotly.graph_objects as go
+
 def multi_plot(df, title, addAll = True):
     fig = go.Figure()
 
@@ -8,24 +9,21 @@ def multi_plot(df, title, addAll = True):
                 x = df.index,
                 y = df[column],
                 name = column
-                
             )
         )
-    
 
     button_all = dict(label = 'All',
                       method = 'update',
-                      args = [{'visible': df.columns.isin([]),
+                      args = [{'visible': [True]*len(df.columns),
                                'title': 'All',
                                'showlegend':True}])
 
     def create_layout_button(column):
         return dict(label = column,
                     method = 'update',
-                    args = [{'visible': df.columns.isin([column]),
+                    args = [{'visible': df.columns==column,
                              'title': column,
                              'showlegend': True}])
-
 
     fig.update_layout(
         updatemenus=[go.layout.Updatemenu(
@@ -33,15 +31,15 @@ def multi_plot(df, title, addAll = True):
             buttons = ([button_all] * addAll) + list(df.columns.map(lambda column: create_layout_button(column)))
             )
         ],
-
     )
+
     # Update remaining layout properties
     fig.update_layout(
         title_text=title,
         height=800
-
     )
 
     fig.show()
+
     
 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 if __name__ == "__main__":
     
     from dryGasAnalysis.DryGasAnalysis import DryGasAnalysis
-    results = DryGasAnalysis(method = 'Nodal', precision = 'implicit', field = 'snøhvit').runAnalysis()
+    results = DryGasAnalysis(method = 'Nodal', field = 'snøhvit').runAnalysis()
     #file_name = 'productionProfile.xlsx'
     #results.to_excel(file_name)
     #import Data.getData as get 
