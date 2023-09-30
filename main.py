b6640682eb5e1683b458216b67b2b3123af90155
Morten Simensen <morten.viersi@gmail.com>
2023-09-30 10:06:14 +0200

    main menu

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 if __name__ == "__main__":
     import streamlit as st
-    #import Plotting.plotFunc as Plot
-    #opt = Plot.dropdown(label = 'What do you want to use the application for?',options = ['NO OPTION CHOSEN', 'FIELD DEVELOPMENT', 'PRODUCTION FORECASTING'], labelVisibility='visible')
-    #if opt == 'FIELD DEVELOPMENT':
-    from dryGasAnalysis.DryGasAnalysis import DryGasAnalysis
+    import Plotting.plotFunc as Plot
+    opt = Plot.dropdown(label = 'What do you want to use the application for?',options = ['NO OPTION CHOSEN', 'FIELD DEVELOPMENT', 'PRODUCTION FORECASTING', 'RESERVOIR PRESSURE FROM PRODUCTION DATA', 'IPR TUNING', 'TPR TUNING'], labelVisibility='visible')
+    if opt == 'FIELD DEVELOPMENT':
+        from dryGasAnalysis.DryGasAnalysis import DryGasAnalysis
+        Analysis = DryGasAnalysis()
+        st.title('Production profile modelling')
+        Analysis.updateFromDropdown()
+        Analysis.run()
+    elif opt == 'PRODUCTION FORECASTING':
+        None
+    elif opt == 'RESERVOIR PRESSURE FROM PRODUCTION DATA':
+        
+        None
+    elif opt == 'IPR TUNING':
+        None
+    elif opt == 'TPR TUNING':
+        None
 
-    Analysis = DryGasAnalysis()
-    st.title('Production profile modelling')
-    Analysis.updateFromDropdown()
-    Analysis.run()
+
+
 
 
 
