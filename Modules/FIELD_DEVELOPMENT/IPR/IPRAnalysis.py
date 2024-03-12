0d11d6c4065a734a5888ddfb384fb342d63cb0b5
Morten Simensen <morten.viersi@gmail.com>
2024-03-12 17:53:48 +0100

    i belive grid is now with const LNGcost

--- Modules/FIELD_DEVELOPMENT/IPR/IPRAnalysis.py
+++ Modules/FIELD_DEVELOPMENT/IPR/IPRAnalysis.py
@@ -1,10 +1,15 @@
 from Data.DefaultData import default_FD_data
 
 def IPRAnalysis(precision: str, parameters: list = default_FD_data()):
-    if precision == 'Explicit':
+    if precision == 'EXPLICIT':
         from Modules.FIELD_DEVELOPMENT.IPR.dfIPRExplicit import IPROnly
-    else: 
+    elif precision == 'IMPLICIT': 
         from Modules.FIELD_DEVELOPMENT.IPR.dfIPRImplicit import IPROnly
+    else:
+        import streamlit as st
+        st.write("Precision is not explicit or implicit")
+        st.write(precision)
+        
     df = IPROnly(*parameters)
     df.columns=('QFieldTarget [sm3/d]', 'qWellTarget[sm3/d]', 'Reservoir pressure [bara]', 'Z-factor', ' Minimum bottomhole pressure [bara]', 'Potential rates per well [sm3/d]', 'Potential field rates [sm3/d]', 'Field rates [sm3/d]', 'Well production rates [sm3/d]', 'yearly gas offtake [sm3]', 'Cumulative gas offtake [sm3]', 'Recovery Factor', 'Bottomhole pressure [bara]')
     df = swapColumns(df, 'QFieldTarget [sm3/d]', 'Field rates [sm3/d]')
