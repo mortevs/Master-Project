c6c5033b95b9061532e993c7270384444cf18a1e
mortevs <86227643+mortevs@users.noreply.github.com>
2024-06-10 12:00:32 +0200

    Update Cache.py

--- Data/Storage/Cache.py
+++ Data/Storage/Cache.py
@@ -64,11 +64,9 @@ class SessionState:
         if not hasattr(st, '_global_session_states'):
             st._global_session_states = {}
         session_state = st._global_session_states.get(id)
-        if session_state is None:
-            session_state = SessionState()  # Initialize new SessionState
-        setattr(session_state, key, value)  # Set the value for the given key
-        st._global_session_states[id] = session_state
-
+        current_list = []
+        current_list.append(value)     
+        setattr(session_state, key, current_list)
 
 def delete_files():
     files_to_delete = [
